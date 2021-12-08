0d0b3168df5c6f2307b3283c9fd7256102d915d0
Aguda <agudaoluwanifemi@gmail.com>
2021-12-08 08:59:25 +0100

    first commit

---
+++ src/Components/dummyData.jsx
@@ -0,0 +1,59 @@
+export const moviesList = [
+    {
+      title: `Venom: Let There Be Carnage`,
+      posterUrl: `https://static.bunnycdn.ru/i/cache/images/4/4b/4b9addbe7d8461d752e2aaea716dcb87.jpg-w380`,
+      description: `Against the backdrop of the uneasy coexistence with Venom, the all-powerful, 
+      shape-shifting extraterrestrial Symbiote, Eddie Brock, an investigative journalist and the entity's 
+      human host, finds himself treading water in his career after the life-altering events of Venom (2018). 
+      Now, to find redemption and put himself on the map, Eddie secures a rare interview with the unhinged 
+      death-row prisoner, Cletus Kasady: a violent, sadistic serial killer left to rot in San Quentin State 
+      Prison. However, as bickering Venom grows increasingly tired of depending on a strict bland diet, 
+      Anne is determined to move on with her life, and an unforeseen complication gives birth to a new 
+      nightmare and a fierce, unstoppable arch-enemy. Is the world prepared for the advent of Carnage, 
+      a red-hued beast with a mind aflame with madness, wrath, and chaos? `,
+      rate: 4,
+    },
+    {
+      title: `Red Notice`,
+      posterUrl: `https://static.bunnycdn.ru/i/cache/images/b/b0/b02d5a16eba363248cf559efab5ba696.jpg-w380`,
+      description: `When an Interpol-issued Red Notice the highest level warrant to hunt and capture the 
+      world's most wanted goes out, the FBI's top profiler John Hartley (Dwayne Johnson) is on the case. 
+      His global pursuit finds him smack dab in the middle of a daring heist where he's forced to partner with
+      the world's greatest art thief Nolan Booth (Ryan Reynolds) in order to catch the world's most wanted 
+      art thief, "The Bishop" (Gal Gadot). The high-flying adventure that ensues takes the trio around the 
+      world, across the dance floor, trapped in a secluded prison, into the jungle and, worst of all for 
+      them, constantly into each other's company.`,
+      rate: 5,
+    },
+    {
+      title: `Big Daddy`,
+      posterUrl: `https://images.moviesanywhere.com/c1fdc9c7100b80161487e4f7a6e9b785/e85118fe-c7ab-466e-b92c-58ee90918b70.jpg`,
+      description: `Fatherhood is brought to another level as Adam Sandler employs his unique brand of 
+      humor to face the challenges of parenthood.`,
+      rate: 5,
+    },
+    {
+      title: `The Tomorrow War`,
+      posterUrl: `https://pics.filmaffinity.com/The_Tomorrow_War-735069980-large.jpg`,
+      description: `Time travelers arrive from 2051 to deliver an urgent message: 30 years in the future 
+      mankind is losing a war against a deadly alien species. The only hope for survival is for soldiers and 
+      civilians to be transported to the future and join the fight. Determined to save the world for his 
+      daughter, Dan Forester teams up with a brilliant scientist and his estranged father to rewrite the 
+      planet’s fate.`,
+      rate: 4,
+    },
+    {
+        title: `Tom Clancy's Without Remorse`,
+        posterUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr9nmZtdp1Z8LTyNq-Ug9R3Ea_Oqizjv0enN82GCbMZ_uLzxqhvsxLNVg0-Pmw7u2ikqQ&usqp=CAU`,
+        description: `An elite Navy SEAL (Michael B. Jordan) uncovers an international conspiracy while 
+        seeking justice for the murder of his pregnant wife in Without Remorse, the explosive origin story 
+        of action hero John Clark`,
+        rate: 3,
+      },
+      {
+        title: `Outlander`,
+        posterUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4cva-1lP7BWHGxzViMxx2HeA9Ah4HAjwkMQGNb8a_zwfxF9Q8`,
+        description: `A man from another world crash lands on Earth during the age of the Vikings.`,
+        rate: 5,
+      },
+  ];
